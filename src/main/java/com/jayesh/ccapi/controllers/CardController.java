8bed115a25a7cc47c27988947ecf6e811d20fb38
Jayesh Gupta <jayeshgupta@Jayeshs-MacBook-Pro.local>
2021-01-27 12:54:45 +0530

    Second Commit, Changed Response type to ResponseEntity

--- src/main/java/com/jayesh/ccapi/controllers/CardController.java
+++ src/main/java/com/jayesh/ccapi/controllers/CardController.java
@@ -9,11 +9,13 @@ import com.jayesh.ccapi.services.CardValidationKit;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
+
+
 
 @RestController
 @RequestMapping("/api/card")
@@ -25,6 +27,7 @@ public class CardController {
 
     @Autowired
     CreditCardRepository creditCardRepository;
+
 
     //Credit Card Get all Cards in Database
     @ApiOperation(value = "View a List of all Credit Cards", response = List.class)
@@ -43,37 +46,41 @@ public class CardController {
             @ApiResponse(code = 409, message = "Credit Card Number already Exists")
     }
     )
-    public ResponseObject createCard(@ApiParam(name = "createCreditCard", value = "Create a Credit Card", required = true) @RequestBody CreditCard card) {
+    public ResponseEntity<ResponseObject> createCard(@ApiParam(name = "createCreditCard", value = "Create a Credit Card", required = true) @RequestBody CreditCard card) {
 
         if (card == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No Object in request");
+            return new ResponseEntity("Object Missing",HttpStatus.BAD_REQUEST);
         }
 
         if (card.getCreditCard() == null || card.getCardlimit() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Credit card or Limit missing in JSON Request");
+            return new ResponseEntity("Credit Card Number or Limit Missing",HttpStatus.BAD_REQUEST);
         }
 
         String c = card.getCreditCard();
         c = c.replaceAll("\\s", "");
 
         if (!CardValidationKit.checkCardFormat(c)) {
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "Credit Card Value in Request is Invalid", card);
+            ResponseObject response = new ResponseObject("Credit Card Value in Request is Invalid", card);
+            return  ResponseEntity.badRequest().body(response);
         }
 
         if (CardValidationKit.amountValidator(card.getCardlimit()) == false) {
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "Limit Value in JSON Response is Invalid", card);
+            ResponseObject response = new ResponseObject("Limit Value in JSON Response is Invalid", card);
+            return  ResponseEntity.badRequest().body(response);
         }
 
         Optional<CreditCard> o = creditCardRepository.findById(c);
         if (o.isPresent()) {
-            return new ResponseObject(HttpStatus.CONFLICT, "Credit Card Already Exists", o.get());
+            ResponseObject response = new ResponseObject( "Credit Card Already Exists", o.get());
+            return  ResponseEntity.status(HttpStatus.CONFLICT).body(response);
         }
 
         String newLimit = "$" + Double.toString(CardValidationKit.amount_from_String(card.getCardlimit()));
 
         CreditCard newCard = new CreditCard(c, newLimit);
         creditCardRepository.save(newCard);
-        return new ResponseObject(HttpStatus.OK, "Request Accepted, Card Created", newCard);
+        ResponseObject response = new ResponseObject( "Request Accepted, Card Created", newCard);
+        return ResponseEntity.ok().body(response);
 
     }
 
@@ -86,28 +93,34 @@ public class CardController {
             @ApiResponse(code = 404, message = "Credit Card Number does not exist")
     }
     )
-    public ResponseObject chargeCard(@ApiParam(name = "CreditCardCharge", value = "Charge to Card", required = true) @RequestBody chargeObject card) {
+    public ResponseEntity chargeCard(@ApiParam(name = "CreditCardCharge", value = "Charge to Card", required = true) @RequestBody chargeObject card) {
         if (card == null) {
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "No JSON Object in Response", null);
+            ResponseObject response = new ResponseObject( "No JSON Object in Response", null);
+            return ResponseEntity.badRequest().body(response);
         }
 
         if (card.getCreditCard() == null || card.getCharge() == null) {
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "JSON has empty Credit Card Number or Charge", card);
+            ResponseObject response = new ResponseObject("JSON has empty Credit Card Number or Charge", card);
+            return ResponseEntity.badRequest().body(response);
+
         }
 
         String cardNumberStr = card.getCreditCard().replaceAll("\\s", "");
 
         if (!CardValidationKit.checkCardFormat(card.getCreditCard())) {
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "Request has Invalid Credit Card Number", card);
+            ResponseObject response =  new ResponseObject("Request has Invalid Credit Card Number", card);
+            return ResponseEntity.badRequest().body(response);
         }
 
         if (!CardValidationKit.amountValidator(card.getCharge())) {
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "Request has Invalid Charge", card);
+            ResponseObject response =  new ResponseObject( "Request has Invalid Charge", card);
+            return ResponseEntity.badRequest().body(response);
         }
 
         Optional<CreditCard> query = creditCardRepository.findById(cardNumberStr);
         if (!query.isPresent()) {
-            return new ResponseObject(HttpStatus.NOT_FOUND, "No Credit Card on record found with given card number", card);
+            ResponseObject response =  new ResponseObject("No Credit Card on record found with given card number", card);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
 
         CreditCard foundCard = query.get();
@@ -116,15 +129,17 @@ public class CardController {
         Double cardCharge = CardValidationKit.amount_from_String(card.getCharge());
 
         if (cardLimit < cardBalance + cardCharge) {
-
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "Charge exceeds Credit Card Limit", card);
+            ResponseObject response =  new ResponseObject( "Charge exceeds Credit Card Limit", card);
+            return ResponseEntity.badRequest().body(response);
         }
         Double newBalance = cardBalance + cardCharge;
         newBalance = Math.round(newBalance * 100.0) / 100.0;
         foundCard.setBalance("$" + newBalance);
 
         creditCardRepository.save(foundCard);
-        return new ResponseObject(HttpStatus.ACCEPTED, "Credit Card charged with request", foundCard);
+        ResponseObject response =  new ResponseObject("Credit Card charged with request", foundCard);
+        return ResponseEntity.ok().body(response);
+
     }
 
     @ApiOperation(value = "Process a payment to a credit card", response = ResponseObject.class)
@@ -135,28 +150,34 @@ public class CardController {
     }
     )
     @PutMapping("/pay")
-    public ResponseObject payCard(@ApiParam(name = "CreditCardPayment", value = "Payment to Card", required = true) @RequestBody payObject card) {
+    public ResponseEntity payCard(@ApiParam(name = "CreditCardPayment", value = "Payment to Card", required = true) @RequestBody payObject card) {
         if (card == null) {
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "No JSON Object in Response", null);
+            ResponseObject response = new ResponseObject("No JSON Object in Response", null);
+            return ResponseEntity.badRequest().body(response);
         }
 
         if (card.getCreditCard() == null || card.getPay() == null) {
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "JSON has empty Credit Card Number or Payment", card);
+            ResponseObject response = new ResponseObject("JSON has empty Credit Card Number or Payment", card);
+            return ResponseEntity.badRequest().body(response);
         }
 
         String cardNumberStr = card.getCreditCard().replaceAll("\\s", "");
 
         if (!CardValidationKit.checkCardFormat(card.getCreditCard())) {
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "Request has Invalid Credit Card Number", card);
+            ResponseObject response = new ResponseObject("Request has Invalid Credit Card Number", card);
+            return ResponseEntity.badRequest().body(response);
         }
 
         if (!CardValidationKit.amountValidator(card.getPay())) {
-            return new ResponseObject(HttpStatus.BAD_REQUEST, "Request has Invalid Payment", card);
+            ResponseObject response = new ResponseObject( "Request has Invalid Payment", card);
+            return ResponseEntity.badRequest().body(response);
+
         }
 
         Optional<CreditCard> query = creditCardRepository.findById(cardNumberStr);
         if (!query.isPresent()) {
-            return new ResponseObject(HttpStatus.NOT_FOUND, "No Credit Card on record found with given card number", card);
+            ResponseObject response = new ResponseObject("No Credit Card on record found with given card number", card);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
 
         CreditCard foundCard = query.get();
@@ -170,6 +191,7 @@ public class CardController {
         foundCard.setBalance("$" + newBalance);
 
         creditCardRepository.save(foundCard);
-        return new ResponseObject(HttpStatus.ACCEPTED, "Credit Card charged with request", foundCard);
+        ResponseObject response = new ResponseObject("Credit Card charged with request", foundCard);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
--- src/main/java/com/jayesh/ccapi/returnObjects/ResponseObject.java
+++ src/main/java/com/jayesh/ccapi/returnObjects/ResponseObject.java
@@ -9,11 +9,6 @@ import org.springframework.stereotype.Component;
 @ApiModel(value = "Response Object" ,description = "Request Response Object")
 public class ResponseObject {
 
-    @ApiModelProperty(notes = "Response Message",required = true,example = "OK",position = 1)
-    private HttpStatus response;
-
-    @ApiModelProperty(notes = "Response Status Code",required = true, example = "200",position = 2)
-    private int Status;
 
     @ApiModelProperty(notes = "Message",required = true,example = "Request Accepted, Card Created",position = 3)
     private String message;
@@ -21,27 +16,10 @@ public class ResponseObject {
     @ApiModelProperty(notes = "Response Object, Specifies the Credit Card Object",required = true,position = 4)
     private Object object;
 
-    public ResponseObject(HttpStatus st, String message, Object object) {
-        this.response = st;
-        this.Status =  st.value();
+    public ResponseObject(String message, Object object) {
+
         this.message = message;
         this.object = object;
-    }
-
-    public HttpStatus getResponse() {
-        return response;
-    }
-
-    public void setResponse(HttpStatus response) {
-        this.response = response;
-    }
-
-    public int getStatus() {
-        return Status;
-    }
-
-    public void setStatus(int status) {
-        Status = status;
     }
 
     public String getMessage() {
